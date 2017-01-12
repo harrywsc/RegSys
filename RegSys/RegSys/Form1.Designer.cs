fb616bcb70aa002192a9af6e73f6c035ac34500b
harrywsc <254790167@qq.com>
2017-01-12 17:15:10 +0800

    完成1

---
+++ RegSys/RegSys/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace RegSys
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1142, 671);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("", System.UriKind.Relative);
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            this.webBrowser1.NewWindow += new System.ComponentModel.CancelEventHandler(this.webBrowser1_NewWindow);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1142, 671);
+            this.Controls.Add(this.webBrowser1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "电子登记系统";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ RegSys/RegSys/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace RegSys
+{
+    public partial class Form1 : Form
+    {
+        int times = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            webBrowser1.IsWebBrowserContextMenuEnabled = false;
+            //webBrowser1.Url = new Uri("http://www.acfun.tv");
+            string [] webPath = new string[1];            
+            ReadPath(@".\FilePath.cfg.xml",webPath);
+            webBrowser1.Navigate(webPath[0]);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (times<10)
+            {
+                times++;
+                MessageBox.Show("窗口禁止关闭！");
+                e.Cancel = true;
+            }
+        }
+
+        private void webBrowser1_NewWindow(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            //将所有的链接的目标，指向本窗体  
+            foreach (HtmlElement archor in this.webBrowser1.Document.Links)
+            {
+                archor.SetAttribute("target", "_self");
+            }
+            //将所有的FORM的提交目标，指向本窗体  
+            foreach (HtmlElement form in this.webBrowser1.Document.Forms)
+            {
+                form.SetAttribute("target", "_self");
+            }
+        }
+        private void ReadPath(string path,string[] webpath)
+        {
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.IgnoreComments = true;//忽略文档里面的注释
+            XmlReader reader = XmlReader.Create(path, settings);
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(reader);
+            XmlNode xn = xmlDoc.SelectSingleNode("File");
+            XmlNodeList xnl = xn.ChildNodes;
+            foreach (XmlNode xn1 in xnl)
+            {
+                XmlElement xe = (XmlElement)xn1;// 将节点转换为元素，便于得到节点的属性值
+                XmlNodeList xnl0 = xe.ChildNodes;
+                webpath[0] = xnl0.Item(0).InnerText;
+            }
+            reader.Close();
+        }
+
+    }
+}
